39309d9c774b755b70f5018d92ebf583949842d0
Elijah Edwards <elijahedwards7598@gmail.com>
2021-08-04 21:12:41 -0700

    Added Models, Controllers, Security Config, and MySQL

--- src/main/java/com/rantapp/MessageRepository.java
+++ src/main/java/com/rantapp/MessageRepository.java
@@ -3,5 +3,5 @@ package com.rantapp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-		Message findByAuthor(String author);
+		Message findById(long Id);
 }
